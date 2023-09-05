cfc485afcf6639de7edbdf6efe6f79cc73b22c12
zaido <zaido@ZAIDALRAFAYIA>
2023-09-05 17:54:20 +0300

    Add project files.

---
+++ cpprojects/cpprojects.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x[3] = { 0 };
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> x[i];
+       
+    }
+    for (int i = 0; i < 3; i++)
+        cout << x[i];
+}
